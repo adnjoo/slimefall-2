266711609001c9f650a5eb81977f871cdade8da5
Andrew Njoo <adnjoo@gmail.com>
2025-03-15 23:14:01 +0700

    run anim (#13)
    * anim
    
    * project

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 @export var player_id: int = -1  # Assigned by ChatRoom
 
@@ -15,19 +15,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		
+
 		# Flip sprite based on direction
 		$AnimatedSprite2D.scale.x = -1 if direction < 0 else 1
 
+		# Play run animation
+		if not $AnimatedSprite2D.is_playing() or $AnimatedSprite2D.animation != "run":
+			$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		# Play idle animation
+		if not $AnimatedSprite2D.is_playing() or $AnimatedSprite2D.animation != "idle":
+			$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
